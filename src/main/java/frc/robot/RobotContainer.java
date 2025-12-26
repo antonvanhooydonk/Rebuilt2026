12e4b89167b8cf23feb29607ca9ae96970af9993
Anton vanHooydonk <antonvanhooydonk@gmail.com>
2025-12-26 12:44:32 -0500

    asdf

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,8 +28,8 @@ import frc.robot.util.Utils;
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot 
+ * (including subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
   // Initialize our controllers
